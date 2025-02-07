4ad597c17040bbcd3ef7f63b5c5f16eb2df098d7
DjohnHK <108556453+DjohnHK@users.noreply.github.com>
2025-02-07 11:56:13 -0300

    Correção HTML

--- script.ps1
+++ script.ps1
@@ -294,7 +294,7 @@ $htmlContent = @"
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Automação DKOS</title>
+    <title>Automacao DKOS</title>
     <style>
         body {
             font-family: Arial, sans-serif;
