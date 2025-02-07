6418c7e82db13d46d02eacc76b694edef11299f8
DjohnHK <108556453+DjohnHK@users.noreply.github.com>
2025-02-07 10:11:37 -0300

    Scripts pós automação

---
+++ Scripts Auxiliares/cleandisk.ps1
@@ -0,0 +1,32 @@
+﻿
+	$objShell = New-Object -ComObject Shell.Application
+	$objFolder = $objShell.Namespace(0xA)
+	$WinTemp = "c:\Windows\Temp\*"
+	
+#1#	Empty Recycle Bin #
+	write-Host "Limpando lixeira" -ForegroundColor Cyan 
+	$objFolder.items() | %{ remove-item $_.path -Recurse -Confirm:$false}
+	
+#2# Remove Temp
+	write-Host "Removendo arquivos temporarios" -ForegroundColor Green
+    Set-Location “C:\Windows\Temp”
+	Remove-Item * -Recurse -Force -ErrorAction SilentlyContinue
+
+    Set-Location “C:\Windows\Prefetch”
+    Remove-Item * -Recurse -Force -ErrorAction SilentlyContinue
+
+    Set-Location “C:\Documents and Settings”
+    Remove-Item “.\*\Local Settings\temp\*” -Recurse -Force -ErrorAction SilentlyContinue
+
+    Set-Location “C:\Users”
+    Remove-Item “.\*\Appdata\Local\Temp\*” -Recurse -Force -ErrorAction SilentlyContinue
+	
+#3# Running Disk Clean up Tool 
+	write-Host "Concluido otimização inicial, executando limpeza de disco" -ForegroundColor Cyan
+	cleanmgr /sagerun:1 | out-Null 
+	
+	$([char]7)
+	Sleep 3
+	write-Host "Limpeza finalizada " -ForegroundColor Yellow 
+	Sleep 3
+##### End of the Script ##### 
---
+++ Scripts Auxiliares/temp-delete.ps1
@@ -0,0 +1,29 @@
+﻿$MyWallpaper="c:\temp\sw\wallpaper.jpg"
+$code = @' 
+using System.Runtime.InteropServices; 
+namespace Win32{ 
+    
+     public class Wallpaper{ 
+        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+         
+         public static void SetWallpaper(string thePath){ 
+            SystemParametersInfo(20,0,thePath,3); 
+         }
+    }
+ } 
+'@
+
+add-type $code 
+[Win32.Wallpaper]::SetWallpaper($MyWallpaper)
+
+xcopy c:\temp\sw\wallpaper.jpg c:\windows
+
+reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "C:\windows\wallpaper.jpg" /f 
+reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 
+
+forfiles /P C:\temp /c "cmd /c del @path /q & rd @path /s /q"
+forfiles /P C:\users\defaultuser0 /c "cmd /c del @path /q & rd @path /s /q"
+shutdown /r /f /t 10
